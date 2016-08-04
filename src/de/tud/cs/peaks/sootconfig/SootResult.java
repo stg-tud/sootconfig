ccb15f8517fea257cac2a56a8e175dfc91fc7457
Ben Hermann <Ben.Hermann@googlemail.com>
2016-08-04 16:13:47 +0200

    Updated Stopwatch call

--- src/de/tud/cs/peaks/sootconfig/SootResult.java
+++ src/de/tud/cs/peaks/sootconfig/SootResult.java
@@ -1,30 +1,36 @@
 package de.tud.cs.peaks.sootconfig;
 
+import com.google.common.base.Stopwatch;
+
 import soot.G;
 import soot.Scene;
 
-import com.google.common.base.Stopwatch;
-
 public class SootResult {
-	private final Stopwatch totalSootRuntime = Stopwatch.createUnstarted();
-	private final Stopwatch classLoadRuntime = Stopwatch.createUnstarted();
-	private final Stopwatch entryPointCalculationRuntime = Stopwatch.createUnstarted();
-	private final Stopwatch packRuntime = Stopwatch.createUnstarted();
+	private final Stopwatch totalSootRuntime = new Stopwatch();
+	private final Stopwatch classLoadRuntime = new Stopwatch();
+	private final Stopwatch entryPointCalculationRuntime = new Stopwatch();
+	private final Stopwatch packRuntime = new Stopwatch();
 
 	private Scene scene;
 
 	private G sootGlobal;
 
-	public G getSootGlobal() { return sootGlobal; }
+	public G getSootGlobal() {
+		return this.sootGlobal;
+	}
 
-	public void setSootGloabel(G global) { this.sootGlobal = global; }
+	public void setSootGloabel(G global) {
+		this.sootGlobal = global;
+	}
 
-	public Scene getScene() { return sootGlobal.soot_Scene(); }
+	public Scene getScene() {
+		return this.sootGlobal.soot_Scene();
+	}
 
 	private String completeOutput;
 
 	public String getCompleteOutput() {
-		return completeOutput;
+		return this.completeOutput;
 	}
 
 	public void setCompleteOutput(String completeOutput) {
